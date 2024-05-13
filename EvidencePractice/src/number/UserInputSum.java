72bc43a861ed41c251a8df10e1a7a3a50e34961d
Shamima-Nasrin10 <shamima48nasrin@gmail.com>
2024-05-13 11:20:40 +0600

    UserInputSum

--- EvidencePractice/src/number/UserInputSum.java
+++ EvidencePractice/src/number/UserInputSum.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package number;
 
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
+
 public class UserInputSum {
-    
+    public static void main(String[] args) {
+        
+        Scanner scanner=new Scanner(System.in);
+        int number=scanner.nextInt();
+        
+        int sum=0;
+        while(number>=0){
+            sum+=number;
+            number=scanner.nextInt();
+        }
+        System.out.println("Sum of values: "+sum);
+        
+    }
 }
