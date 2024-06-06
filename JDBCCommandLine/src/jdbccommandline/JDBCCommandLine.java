9a4fcdeea7730a3dc919b4cd60dc9284faea7e39
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-06-06 18:37:41 +0600

    CommandLine

--- JDBCCommandLine/src/jdbccommandline/JDBCCommandLine.java
+++ JDBCCommandLine/src/jdbccommandline/JDBCCommandLine.java
@@ -1,7 +1,6 @@
 package jdbccommandline;
 
 import entity.Student;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,36 +11,37 @@ import java.util.logging.Logger;
 import util.DbUtil;
 
 public class JDBCCommandLine {
-    
+
     static PreparedStatement ps;
     static ResultSet rs;
     static DbUtil dbUtil = new DbUtil();
-    
+
     public static void main(String[] args) {
         Student student = new Student();
-        List<Student> studentList=getById(1);
-        
+        List<Student> studentList = getById(1);
+
         student.setName(studentList.get(0).getName());
         student.setEmail(studentList.get(0).getEmail());
         student.setAddress(studentList.get(0).getAddress());
         student.setCell("01521499594");
         student.setId(studentList.get(0).getId());
-        
+
         editData(student);
         showData();
+        
     }
-    
+
     public static void saveData() {
         String insertSql = "insert into student(name, email, address, cell) "
                 + "values(?,?,?,?)";
-        
+
         try {
             ps = dbUtil.getCon().prepareStatement(insertSql);
             ps.setString(1, "Shamima");
             ps.setString(2, "nasrin05shamima@gmail.com");
             ps.setString(3, "Kollyanpur");
             ps.setString(4, "01838020680");
-            
+
             ps.executeUpdate();
             ps.close();
             dbUtil.getCon().close();
@@ -49,15 +49,15 @@ public class JDBCCommandLine {
             Logger.getLogger(JDBCCommandLine.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public static void showData() {
-        
+
         String selectSql = "select * from student";
-        
+
         try {
             ps = dbUtil.getCon().prepareStatement(selectSql);
             rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
@@ -69,50 +69,50 @@ public class JDBCCommandLine {
 //                        + address + "\n" + "Cell No: " + cell+"\n");
                 System.out.printf("Id     :%d\nName   :%s\nEmail  :%s\nAddress:%s\nCell No:%s\n\n",
                         id, name, email, address, cell);
-                
+
             }
             ps.close();
             rs.close();
             dbUtil.getCon().close();
-            
+
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(JDBCCommandLine.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
     public static void editData(Student s) {
         String sql = "update student set name=?, email=?, address=?, cell=? where id=?";
-        
+
         try {
             ps = dbUtil.getCon().prepareStatement(sql);
-            
+
             ps.setString(1, s.getName());
             ps.setString(2, s.getEmail());
             ps.setString(3, s.getAddress());
             ps.setString(4, s.getCell());
             ps.setInt(5, s.getId());
-            
+
             ps.executeUpdate();
             ps.close();
             dbUtil.getCon().close();
-            
+
             System.out.println("Edit successful");
-            
+
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(JDBCCommandLine.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public static List<Student> getById(int id) {
         List<Student> studentList = new ArrayList<>();
         String sql = "select * from student where id=?";
-        
+
         try {
             ps = dbUtil.getCon().prepareStatement(sql);
             ps.setInt(1, id);
             rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 Student s = new Student(
                         rs.getInt("id"),
@@ -121,14 +121,14 @@ public class JDBCCommandLine {
                         rs.getString("address"),
                         rs.getString("cell")
                 );
-                
+
                 studentList.add(s);
             }
-            
+
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(JDBCCommandLine.class.getName()).log(Level.SEVERE, null, ex);
         }
         return studentList;
     }
-    
+
 }
