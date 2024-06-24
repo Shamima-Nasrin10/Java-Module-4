5ae9b68e63953b7d9982ae0357f6a5e8dd15556a
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-06-24 15:15:03 +0600

    evidence

--- HackerRankProblem/src/hackerrankproblem/AnagramTwoString.java
+++ HackerRankProblem/src/hackerrankproblem/AnagramTwoString.java
@@ -8,7 +8,6 @@ public class AnagramTwoString {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter first string");
-        String 
         System.out.println("Enter second string");
     }
     
